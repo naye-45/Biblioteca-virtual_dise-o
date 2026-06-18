8ed5f74e863f334009520ab5d27f42e16e16fa77
Mateo-ortiz007 <elsenadordelbanco34@gmail.com>
2026-06-18 12:17:26 -0500

    Cambios en login o UI

---
+++ login/login.js
@@ -0,0 +1,41 @@
+const loginForm = document.getElementById("loginForm");
+const mensaje = document.getElementById("mensaje");
+
+/*
+    Usuario único permitido
+*/
+const usuarioPermitido = {
+    usuario: "bibliotecario",
+    password: "12345"
+};
+
+/*
+    Login
+*/
+loginForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const usuario = document.getElementById("usuario").value;
+    const password = document.getElementById("password").value;
+
+    if (
+        usuario === usuarioPermitido.usuario &&
+        password === usuarioPermitido.password
+    ) {
+
+        localStorage.setItem("sesionActiva", "true");
+        localStorage.setItem("usuario", usuario);
+
+        // RUTA VACÍA PARA QUE OTROS COLABORADORES LA DEFINAN
+        const RUTA_HOME = "";
+
+        window.location.href = RUTA_HOME;
+
+    } else {
+
+        mensaje.textContent = "Usuario o contraseña incorrectos";
+        mensaje.className = "error";
+    }
+
+});
